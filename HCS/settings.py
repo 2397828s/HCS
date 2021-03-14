b4f334b74ae475515dd8d9f3d7af0f587023a6c5
2397828s <2397828s@student.gla.ac.uk>
2021-03-14 04:54:38 +0000

    host setting

--- HCS/settings.py
+++ HCS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k-c2lup79f_sa2o2!dr8y(kgfamzfy_a6w9=ox!3ah)q@djxkq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['747736317.pythonanywhere.com']
 
 
 # Application definition
